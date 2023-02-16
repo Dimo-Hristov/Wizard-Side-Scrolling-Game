344abddd74d981f7762bed252072c77c8e25e1c5
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-02-16 12:17:21 +0100

    add keys registration

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 let state = initState();
 let game = initGameObjects();
 
+const availableKeys = ['keyA', 'keyS', 'keyD', 'keyW']
+
+document.addEventListener('keydown', (e) => {
+    if (availableKeys.includes(e.code)) {
+        state.keys[e.code] = true;
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    if (availableKeys.includes(e.code)) {
+        state.keys[e.code] = false;
+    }
+})
+
+
 game.startScreen.addEventListener('click', (e) => {
     // change the screens
     game.startScreen.classList.add('hidden');
